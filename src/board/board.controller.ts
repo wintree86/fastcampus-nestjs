e8f577ddb91d54f36364920c47784f752c166950
wintree86 <mith1004@gmail.com>
2023-07-13 04:03:46 +0900

    Ch03 - 06 Routing

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+
+    @Get()
+    findAll() {
+        return 'findAll';
+    }
+
+    @Get(':id')
+    find(
+        @Param('id') id: number
+    ) {
+        return `find id:${id}`;
+    }
+
+    @Post()
+    create(
+        @Body() data
+    ) {
+        return 'create';
+    }
+
+    @Put(':id')
+    update(
+        @Param('id') id: number,
+        @Body() data
+    ) {
+        return 'update';
+    }
+
+
+    @Delete(':id')
+    remove(
+        @Param('id') id: number
+    ) {
+        return 'delete';
+    }
+}
