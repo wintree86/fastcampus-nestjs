f812a1dddd9a4131102cf0a30e22f90501392dc8
wintree86 <mith1004@gmail.com>
2023-07-19 04:29:08 +0900

    Ch03 - 07 Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardController } from './board/board.controller';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [],
+  imports: [BoardModule],
   controllers: [AppController, BoardController],
   providers: [AppService],
 })
