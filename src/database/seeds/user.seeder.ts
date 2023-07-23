fe65cdf5a35c56d3f9f029a315471b76bd06e476
wintree86 <mith1004@gmail.com>
2023-07-24 06:04:13 +0900

    Ch05 - 06 TypeORM Seeding

---
+++ src/database/seeds/user.seeder.ts
@@ -0,0 +1,20 @@
+import { User } from 'src/entity/user.entity';
+import { DataSource } from 'typeorm';
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+
+export default class UserSeeder implements Seeder {
+  async run(
+    dataSource: DataSource,
+    factoryManager: SeederFactoryManager,
+  ): Promise<any> {
+    const repository = dataSource.getRepository(User);
+
+    await repository.insert([
+      {
+        username: 'fastcampus',
+        name: 'hong gildong',
+        password: 'fastcampus1234',
+      },
+    ]);
+  }
+}
