2d5507d682a113862a6c3c1289a9671fa1f13a32
wintree86 <mith1004@gmail.com>
2023-07-23 20:18:06 +0900

    Ch05 - 2 TypeORM 셋팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { LoggingMiddleware } from './middleware/logging.middleware';
 import ConfigModule from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
     ConfigModule(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
     BoardModule
   ],
   controllers: [AppController],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,8 @@
 export default () => ({
   ENVIRONMENT: process.env.ENVIRONMENT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
 });
