1af597d5b1de6ba23d8615b7d0aa42d194730ad5
wintree86 <mith1004@gmail.com>
2023-07-31 01:55:23 +0900

    Ch07 - 04 E2E

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,6 @@
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
 
-console.log(process.env.NODE_ENV)
 export default () => ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: `.env.${process.env.NODE_ENV}`,
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('e2e 테스트', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,37 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('AppController', () => {
+    it('/ (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello World!');
+    });
+
+    it('/name?name=fastcampus (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/name?name=fastcampus')
+        .expect(200)
+        .expect('fastcampus hello');
+    });
+
+    it('[로그인]', () => {
+      return request(app.getHttpServer())
+        .post('/login')
+        .send({
+          username: 'fastcampus2024',
+          password: 'fastcampuspassword',
+        })
+        .expect(201);
+    });
   });
+
+  describe('BoardController', () => {
+    it('게시글 가져오기', () => {
+      return request(app.getHttpServer()).get('/board').expect(200);
+    });
+  });
+
+  describe('UserController', () => {});
 });
